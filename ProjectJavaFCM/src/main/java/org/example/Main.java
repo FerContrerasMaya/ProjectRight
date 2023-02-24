53bf60dcd2ee2d2dc17806ce3ce6c7ed781c4831
FerContrerasMaya <fercontreras060405@gmail.com>
2023-02-24 10:39:12 -0600

    Practica 1

--- ProjectJavaFCM/src/main/java/org/example/Main.java
+++ ProjectJavaFCM/src/main/java/org/example/Main.java
@@ -37,7 +37,7 @@ public class Main {
     //}
 //Desarrollar un programa que le pida al usuario su nombre, edad, peso y altura, calcule el imc
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
 
         String nombre;
         int edad;
@@ -67,5 +67,77 @@ public class Main {
         System.out.println("El peso es: " + peso);
         System.out.println("La altura es:" + altura);
         System.out.println("El imc es: " +imc);
+    } */
+
+    //Desarrollar un programa que le pida al usuario su nombre, apellido, docimicilio, cuanto cotizo en 2022, cuantos impuestos va a pagar.
+    //Declarar constante de isr - 12%
+
+    /*public static void main(String[] args) {
+
+        String nombre;
+        String apellido;
+        String domiclio;
+        double cotizo;
+        final double isr = 0.12;
+        double isrUser;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nombre: ");
+        nombre = scanner.nextLine();
+
+        System.out.println("Apellido: ");
+        apellido = scanner.nextLine();
+
+        System.out.println("Domicilio: ");
+        domiclio = scanner.nextLine();
+
+        System.out.println("Cotizo: ");
+        cotizo = scanner.nextDouble();
+
+        isrUser = cotizo * isr;
+        System.out.println("El impuesto es = " + isrUser);
+
+        System.out.println("El nombre es: " + nombre);
+        System.out.println("El apellido es: M" + apellido);
+        System.out.println("El domicilio es: " + domiclio);
+        System.out.println("El impuesto es: " + isrUser);
+        System.out.println("El isr es de: %" + isr*100);
+
+    } */
+// 1. Pedir al usuario nombre, apellido, domicilio y precio de su carro
+// 2. Calcular el impuesto de tenencia que es igual a 1.3% de lo que cuesta el carro
+    public static void main(String[] args) {
+
+        String nombre;
+        String apellido;
+        String domicilio;
+        int carro;
+        final double tenencia= 0.013;
+        double tenenciaUser;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nombre: ");
+        nombre = scanner.nextLine();
+
+        System.out.println("Apellido: ");
+        apellido= scanner.nextLine();
+
+        System.out.println("Domicilio: ");
+        domicilio = scanner.nextLine();
+
+        System.out.println("Carro: ");
+        carro = scanner.nextInt();
+
+        tenenciaUser=carro*tenencia;
+
+        System.out.println("El nombre es: " + nombre);
+        System.out.println("El apellido es: " + apellido);
+        System.out.println("El domicilio es: " + domicilio);
+        System.out.println("La tenencia es: " + tenenciaUser);
+
     }
+
 }
