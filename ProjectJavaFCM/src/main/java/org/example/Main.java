972e4b9655f4abf2098d1f3853a786d6f90054cb
FerContrerasMaya <fercontreras060405@gmail.com>
2023-03-01 10:33:54 -0600

    Ejercicio 2

--- ProjectJavaFCM/src/main/java/org/example/Main.java
+++ ProjectJavaFCM/src/main/java/org/example/Main.java
@@ -1,152 +1,49 @@
 package org.example;
 import java.util.*;
 public class Main {
-    //public static void main(String[] args) {
+    public static void main (String[] args) {
+        //Desarrolla un programa que le pregunte al usuario 3 numeros
+        // describe cual es mayor de los 3
 
-        /* System.out.println("Hello world!");
-        System.out.println("Fernanda Contreras Maya");
-        int mientero = 10;
-        float mipuntoflotante = 0.5f;
-        double midecimal = 22.5;
-        String mitexto = "Hola soy un texto en java";
-        char micaracter = 'a';
+/*
+        int numero1;
+        int numero2;
+        int numero3;
+        int numero4;
 
-        Scanner  scanner = new Scanner(System.in);
-
-        int numero;
-        numero = scanner.nextInt();
-
-        System.out.println("mi numero que acabo de ingresar es: " + numero); */
-
-    //}
-    // Desarrollar un progrmama en java que lea dos numeros, los sume y me muentre el resultado
-
-   /* public static void main (String[] args) {
-
-        int numero;
-        int nomero;
-        int suma;
         Scanner scanner = new Scanner(System.in);
 
-        nomero = scanner.nextInt();
-        numero = scanner.nextInt();
-        suma = numero + nomero;
-        System.out.println("La suma de estos numeros es= " + suma);
-    }
+        System.out.println("Numero 1: ");
+        numero1 = scanner.nextInt();
+
+        System.out.println("Numero 2: ");
+        numero2 = scanner.nextInt();
+
+        System.out.println("Numero 3: ");
+        numero3 = scanner.nextInt();
+
+        System.out.println("Numero 4: ");
+        numero4 = scanner.nextInt();
+
+        if (numero1 > numero2 && numero1 > numero3 && numero1 > numero4) {
+            System.out.println("El numero 1 es mayor");
+        }
+        if (numero2 > numero1 && numero2 > numero3 && numero2 > numero4) {
+            System.out.println("El numero 2 es mayor");
+        }
+        if (numero3 > numero1 && numero3 > numero2 && numero3 > numero4) {
+            System.out.println("El numero 3 es mayor");
+        }
+        if (numero4 > numero1 && numero4 > numero2 && numero4 > numero3) {
+            System.out.println("El numero 4 es mayor");
+        }
+        if (numero1 == numero2 && numero1 == numero3 && numero1 == numero4) {
+            System.out.println("Los numeros son iguales");
+        }
+
 */
-    //}
-//Desarrollar un programa que le pida al usuario su nombre, edad, peso y altura, calcule el imc
-
-   /* public static void main(String[] args) {
-
-        String nombre;
-        int edad;
-        int peso;
-        double altura;
-        double imc;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Nombre: ");
-        nombre = scanner.nextLine();
-
-        System.out.println("Edad: ");
-        edad = scanner.nextInt();
-
-        System.out.println("Peso: ");
-        peso = scanner.nextInt();
-
-        System.out.println("Altura: ");
-        altura = scanner.nextDouble();
-
-        imc = peso/(altura*altura);
-        System.out.println("El imc es= " + imc);
-
-        System.out.println("El nombre es: " + nombre);
-        System.out.println("La edad es:" + edad );
-        System.out.println("El peso es: " + peso);
-        System.out.println("La altura es:" + altura);
-        System.out.println("El imc es: " +imc);
-    } */
-
-    //Desarrollar un programa que le pida al usuario su nombre, apellido, docimicilio, cuanto cotizo en 2022, cuantos impuestos va a pagar.
-    //Declarar constante de isr - 12%
-
-    /*public static void main(String[] args) {
-
-        String nombre;
-        String apellido;
-        String domiclio;
-        double cotizo;
-        final double isr = 0.12;
-        double isrUser;
-
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Nombre: ");
-        nombre = scanner.nextLine();
-
-        System.out.println("Apellido: ");
-        apellido = scanner.nextLine();
-
-        System.out.println("Domicilio: ");
-        domiclio = scanner.nextLine();
-
-        System.out.println("Cotizo: ");
-        cotizo = scanner.nextDouble();
-
-        isrUser = cotizo * isr;
-        System.out.println("El impuesto es = " + isrUser);
-
-        System.out.println("El nombre es: " + nombre);
-        System.out.println("El apellido es: M" + apellido);
-        System.out.println("El domicilio es: " + domiclio);
-        System.out.println("El impuesto es: " + isrUser);
-        System.out.println("El isr es de: %" + isr*100);
-
-    } */
-// 1. Pedir al usuario nombre, apellido, domicilio y precio de su carro
-// 2. Calcular el impuesto de tenencia que es igual a 1.3% de lo que cuesta el carro
-    /*public static void main(String[] args) {
-
-        String nombre;
-        String apellido;
-        String domicilio;
-        int carro;
-        final double tenencia= 0.013;
-        double tenenciaUser;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Nombre: ");
-        nombre = scanner.nextLine();
-
-        System.out.println("Apellido: ");
-        apellido= scanner.nextLine();
-
-        System.out.println("Domicilio: ");
-        domicilio = scanner.nextLine();
-
-        System.out.println("Carro: ");
-        carro = scanner.nextInt();
-
-        tenenciaUser=carro*tenencia;
-
-        System.out.println("El nombre es: " + nombre);
-        System.out.println("El apellido es: " + apellido);
-        System.out.println("El domicilio es: " + domicilio);
-        System.out.println("La tenencia es: " + tenenciaUser);
-
-    } */
-    public static void main(String[] args) {
-        int a=10;
-        int b=5;
-        int c=3;
-
-        if(a>b){
-            System.out.println("a es mayor que b");
-        }else{
-            System.out.println("a no es mayor que b");}
+        for (int i = 0; i < 10 ; i++){
+            System.out.println("Int vale: "+i);
+        }
     }
-}
+    }
---
+++ Python/Ejercicio 2.py
@@ -0,0 +1,7 @@
+numero = input ("Ingresa un numero: ")
+numero = int (numero)
+
+if numero > 100 and numero < 1000000:
+    print ("Correcto")
+else :
+    print ("Incorrecto")
