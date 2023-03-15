059af224a9259b490a98826a0ef7de01b29e0e98
FerContrerasMaya <fercontreras060405@gmail.com>
2023-03-15 12:01:45 -0600

    Ejercicios de python

---
+++ Python/Ejercicio7.py
@@ -0,0 +1,20 @@
+lado = input ("Cuantos lados tiene tu figura?: ")
+lado = int (lado)
+
+if (lado == 3) :
+    print ("Triangulo")
+if (lado == 4) :
+    print ("Cuadrado")
+if (lado == 5) :
+    print ("Pentagono")
+if (lado == 6) :
+    print ("Hexagono")
+if (lado == 7) :
+    print ("Heptagono")
+if (lado == 8) :
+    print ("Octagono")
+if (lado == 10) :
+    print ("Decagono")
+if (lado == 12) :
+    print ("Dodecagono")
+
---
+++ Python/Ejercicio9.py
@@ -0,0 +1,16 @@
+num = input ("Dime un numero del 1 al 100: ")
+num = int (num)
+
+if (num >= 1 and num <= 20):
+    print ("Rojo")
+if (num >= 21 and num <= 40):
+    print ("Amarillo")
+if (num >= 41 and num <= 60):
+    print ("Naranja")
+if (num >= 61 and num <= 80):
+    print ("Azul")
+if (num >= 81 and num <= 100):
+    print ("Verde")
+if (num > 100) :
+    print ("Error")
+
